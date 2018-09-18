cd27fb6569c3b39d29dfc227ca18680618b5f4fd
Cory Ma <cory.ma@gmail.com>
2018-09-18 21:25:58 +0800

    Error handler

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 import VisualRecognitionV3 from 'watson-developer-cloud/visual-recognition/v3'
 import BoxSDK from 'box-node-sdk'
-import endpoint  from './endpoint.json'
+import endpoint from './endpoint.json'
 import sortBy from 'sort-by'
 
 const boxConfig = endpoint.boxConfig
@@ -27,30 +27,39 @@ var visualRecognition = new VisualRecognitionV3({
 
 //Fetach the file items in the folder
 boxClient.folders.getItems(folderID, null, (err, items) => {
-    for (const item of items.buffer) {
-        let fileID = item.id
-       
-        //Read file stream
-        boxClient.files.getReadStream(fileID, null, (err, stream) => {
-         
-            const params = {
-                images_file: stream,
-                threshold: watsonConfig.threshold,
-                owners: watsonConfig.api_owner
-            }
-      
-            visualRecognition.classify(params, (err, res) => {
-                console.log(res.images[0].classifiers[0])
-                if (err) {
-                    console.error("visualRecognition.classify Error!", err)
-                } else {
-                    var watsonTags = res.images[0].classifiers[0].classes.sort(sortBy('-score')).slice(0, 5).map((item, index, array) => {
-                        return `${item.class} `
-                    })
-                    boxClient.files.update(fileID, { "tags": watsonTags })
+    if (err) {
+        console.log(err)
+    } else {
+        for (const item of items.buffer) {
+            let fileID = item.id
+
+            //Read file stream
+            boxClient.files.getReadStream(fileID, null, (err, stream) => {
+
+                const params = {
+                    images_file: stream,
+                    threshold: watsonConfig.threshold,
+                    owners: watsonConfig.api_owner
                 }
+
+                visualRecognition.classify(params, (err, res) => {
+
+                    if (err) {
+                        console.error("visualRecognition.classify Error!", err)
+                    } else {
+                        console.log(res.images[0].classifiers[0])
+                        let watsonTags = res.images[0].classifiers[0].classes
+                        watsonTags = watsonTags.sort(
+                            sortBy('-score')).slice(0, 5).map((item) => {
+                                return `${item.class} `
+                            })
+
+                        boxClient.files.update(fileID, { "tags": watsonTags })
+                    }
+                })
             })
-        })
+        }
     }
+
 })
 
